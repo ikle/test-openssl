93643dbb3f2c26c7d9440e3f1c08fdb51142a05b
Alexei A. Smekalkine <ikle@ikle.ru>
2018-10-19 10:13:10 +0300

    crypto-verify: support public keys from X.509 certificates

--- crypto-verify.c
+++ crypto-verify.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <openssl/evp.h>
 #include <openssl/pem.h>
+#include <openssl/x509.h>
 
 #include "crypto-verify.h"
 
@@ -9,12 +10,23 @@ struct pkey *pkey_read_public(const char *filename, const char *password)
 {
 	FILE *f;
 	EVP_PKEY *key;
+	X509 *cert;
 
 	if ((f = fopen(filename, "rb")) == NULL)
 		return NULL;
 
 	key = PEM_read_PUBKEY(f, NULL, NULL, (void *) password);
+	if (key != NULL)
+		goto out;
 
+	rewind(f);
+
+	if ((cert = PEM_read_X509(f, NULL, NULL, (void *) password)) == NULL)
+		goto out;
+
+	key = X509_get_pubkey(cert);
+	X509_free(cert);
+out:
 	fclose(f);
 	return (void *) key;
 }

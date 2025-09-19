56115aeec89a15e81d961eb9647b2e457e04909a
Alexei A. Smekalkine <ikle@ikle.ru>
2025-09-19 21:16:03 +0300

    crypto-verify: add headers and unify code style

--- crypto-verify.c
+++ crypto-verify.c
@@ -1,3 +1,11 @@
+/*
+ * OpenSSL Crypto Verify Helpers
+ *
+ * Copyright (c) 2015-2025 Alexei A. Smekalkine <ikle@ikle.ru>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <openssl/evp.h>
@@ -6,48 +14,48 @@
 
 #include "crypto-verify.h"
 
-struct pkey *pkey_read_public(const char *filename, const char *password)
+struct pkey *pkey_read_public (const char *filename, const char *password)
 {
 	FILE *f;
 	EVP_PKEY *key;
 	X509 *cert;
 
-	if ((f = fopen(filename, "rb")) == NULL)
+	if ((f = fopen (filename, "rb")) == NULL)
 		return NULL;
 
-	key = PEM_read_PUBKEY(f, NULL, NULL, (void *) password);
+	key = PEM_read_PUBKEY (f, NULL, NULL, (void *) password);
 	if (key != NULL)
 		goto out;
 
-	rewind(f);
+	rewind (f);
 
-	if ((cert = PEM_read_X509(f, NULL, NULL, (void *) password)) == NULL)
+	if ((cert = PEM_read_X509 (f, NULL, NULL, (void *) password)) == NULL)
 		goto out;
 
-	key = X509_get_pubkey(cert);
-	X509_free(cert);
+	key = X509_get_pubkey (cert);
+	X509_free (cert);
 out:
-	fclose(f);
+	fclose (f);
 	return (void *) key;
 }
 
-int verify_init(struct verify_ctx *c, const char *digest)
+int verify_init (struct verify_ctx *c, const char *digest)
 {
 	const EVP_MD *md;
 
-	if ((md = EVP_get_digestbyname(digest)) == NULL)
+	if ((md = EVP_get_digestbyname (digest)) == NULL)
 		return 0;
 
-	EVP_MD_CTX_init(&c->c);
+	EVP_MD_CTX_init (&c->c);
 
-	return EVP_VerifyInit_ex(&c->c, md, NULL);
+	return EVP_VerifyInit_ex (&c->c, md, NULL);
 }
 
-int verify_final(struct verify_ctx *c, const void *sign, size_t size,
-		 struct pkey *key)
+int verify_final (struct verify_ctx *c, const void *sign, size_t size,
+		  struct pkey *key)
 {
-	int status = EVP_VerifyFinal(&c->c, sign, size, (void *) key);
+	int status = EVP_VerifyFinal (&c->c, sign, size, (void *) key);
 
-	EVP_MD_CTX_cleanup(&c->c);
+	EVP_MD_CTX_cleanup (&c->c);
 	return status == 1;
 }
--- crypto-verify.h
+++ crypto-verify.h
@@ -1,33 +1,41 @@
-#ifndef _CRYPTO_VERIFY_H
-#define _CRYPTO_VERIFY_H  1
+/*
+ * OpenSSL Crypto Verify Helpers
+ *
+ * Copyright (c) 2015-2025 Alexei A. Smekalkine <ikle@ikle.ru>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef CRYPTO_VERIFY_H
+#define CRYPTO_VERIFY_H  1
 
 #include <openssl/evp.h>
 
-struct pkey *pkey_read_public(const char *filename, const char *password);
+struct pkey *pkey_read_public (const char *filename, const char *password);
 
-static inline void pkey_free(struct pkey *key)
+static inline void pkey_free (struct pkey *key)
 {
-	EVP_PKEY_free((void *) key);
+	EVP_PKEY_free ((void *) key);
 }
 
 static inline size_t pkey_size(struct pkey *key)
 {
-	return EVP_PKEY_size((void *) key);
+	return EVP_PKEY_size ((void *) key);
 }
 
 struct verify_ctx {
 	EVP_MD_CTX c;
 };
 
-int verify_init(struct verify_ctx *c, const char *digest);
+int verify_init (struct verify_ctx *c, const char *digest);
 
 static inline
-int verify_update(struct verify_ctx *c, const void *data, size_t size)
+int verify_update (struct verify_ctx *c, const void *data, size_t size)
 {
-	return EVP_VerifyUpdate(&c->c, data, size);
+	return EVP_VerifyUpdate (&c->c, data, size);
 }
 
-int verify_final(struct verify_ctx *c, const void *sign, size_t size,
-		 struct pkey *key);
+int verify_final (struct verify_ctx *c, const void *sign, size_t size,
+		  struct pkey *key);
 
-#endif  /* _CRYPTO_VERIFY_H */
+#endif  /* CRYPTO_VERIFY_H */
